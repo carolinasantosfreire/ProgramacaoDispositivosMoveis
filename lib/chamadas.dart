41dc070bf110bc2433cb49c30bb0c562a2cff06b
carolinasantosfreire <126828127+carolinasantosfreire@users.noreply.github.com>
2023-05-18 14:22:54 -0400

    chamada aula 18/05

--- lib/chamadas.dart
+++ lib/chamadas.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -8,9 +7,44 @@ class Chamadas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text("Página de Chamadas"),
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("Criar link de chamada"),
+          subtitle: Text("Compartilhar um link para sua chamada do WhatsApp"),
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 55, 128, 58),
+            child: Icon(Icons.link),
+          ),
+        ),
+        ListTile(
+          title: Text("Recentes"),
+        ),
+        ListTile(
+          title: Text("Bella Ramsey❤️"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.call_made,
+                color: Color.fromARGB(255, 74, 174, 204),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Hoje 08:35"),
+            ],
+          ),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAoD900rlP_dWKQ0g7pDm75iakIAsfBrcf_A&usqp=CAU"),
+          ),
+          trailing: Icon(
+            Icons.call,
+            color: Color.fromARGB(255, 52, 143, 60),
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/conversas.dart
+++ lib/conversas.dart
@@ -8,64 +8,141 @@ class Conversas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-        children: [
-          ListTile(
-            title: Text("Bella Ramsey❤️"),
-            subtitle: Row(
-              children: [
-                Icon(Icons.done_all, color: Color.fromARGB(255, 74, 174, 204), size: 18,),
-                SizedBox(width: 4,),
-                Text("Hii, baby💗"),
-              ],
-            ),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAoD900rlP_dWKQ0g7pDm75iakIAsfBrcf_A&usqp=CAU"),
-            ),
-            trailing: Text("08:45"),
+      children: [
+        ListTile(
+          title: Text("Bella Ramsey❤️"),
+          subtitle: Text("Hii, baby💗"),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAoD900rlP_dWKQ0g7pDm75iakIAsfBrcf_A&usqp=CAU"),
           ),
-          ListTile(
-            title: Text("Izaa💙"),
-            subtitle: Row(
-              children: [
-                Icon(Icons.done_all, color: Color.fromARGB(255, 152, 159, 161), size: 18,),
-                SizedBox(width: 4,),
-                Text("Amiga, vai hoje para o IF??"),
-              ],
-            ), 
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage("https://images.unsplash.com/photo-1554147687-e471aee1ff5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"),
-            ),
-            trailing: Text("10:35"),
+          trailing: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("08:45",
+              style: TextStyle(
+                  color: Color.fromARGB(223, 35, 170, 17),
+                ),
+              ), 
+              SizedBox(height: 5),
+              CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 10,
+                child: Text("2",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+                ),
+              )
+            ],
           ),
-          ListTile(
-            title: Text("Grupo da Família"),
-            subtitle: Text("Olá, família!!..."),
-            leading: Icon(Icons.account_circle, size: 48,),
-            trailing: Text("11:23"),
+        ),
+        ListTile(
+          title: Text("Izaa💙"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                color: Color.fromARGB(255, 74, 174, 204),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Amiga, vai hoje para o IF??"),
+            ],
           ),
-          ListTile(
-            title: Text("Grupo do TCC"),
-            subtitle: Text("Me enviem a introdução até as 22h!"),
-            leading: Icon(Icons.account_circle, size: 48,),
-            trailing: Text("12:00"),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1554147687-e471aee1ff5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"),
           ),
-          ListTile(
-            title: Text("Maria Luysa💕"),
-            subtitle: Text("Amigaaa, nem te conto..."),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage("https://images.unsplash.com/photo-1548550023-2bdb3c5beed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
-            ),
-            trailing: Text("16:00"),
+          trailing: Text("10:35"),
+        ),
+        ListTile(
+          title: Text("Grupo da Família"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                color: Color.fromARGB(255, 152, 159, 161),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Olá, família!!..."),
+            ],
           ),
-          ListTile(
-            title: Text("Luís Rodolfo❤️"),
-            subtitle: Text("Amigo, vamo sair hoje?"),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage("https://images.unsplash.com/photo-1676322559474-91c24a7e6a1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
-            ),
-            trailing: Text("17:25"),
+          leading: Icon(
+            Icons.account_circle,
+            size: 48,
           ),
-        ],
-      );
+          trailing: Text("11:23"),
+        ),
+        ListTile(
+          title: Text("Grupo do TCC"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                color: Color.fromARGB(255, 74, 174, 204),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Me enviem a introdução até as 22h!"),
+            ],
+          ),
+          leading: Icon(
+            Icons.account_circle,
+            size: 48,
+          ),
+          trailing: Text("12:00"),
+        ),
+        ListTile(
+          title: Text("Maria Luysa💕"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                color: Color.fromARGB(255, 74, 174, 204),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Amigaaa, nem te conto..."),
+            ],
+          ),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1548550023-2bdb3c5beed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+          ),
+          trailing: Text("16:00"),
+        ),
+        ListTile(
+          title: Text("Luís Rodolfo❤️"),
+          subtitle: Row(
+            children: [
+              Icon(
+                Icons.done_all,
+                color: Color.fromARGB(255, 74, 174, 204),
+                size: 18,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text("Amigo, vamo sair hoje?"),
+            ],
+          ),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.unsplash.com/photo-1676322559474-91c24a7e6a1c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"),
+          ),
+          trailing: Text("17:25"),
+        ),
+      ],
+    );
   }
 }
